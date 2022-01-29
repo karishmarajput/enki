347cfe6cc9209b72378fb5606b7b756b20a46ac9
Joel Varghese <joel02varghese@gmail.com>
2022-01-29 17:36:22 +0530

    Add Login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Login from "./Login";
+import Moralis from 'moralis/dist/moralis.min.js';
+
 function App() {
+  const serverUrl = "https://a8kyvifx3fa2.usemoralis.com:2053/server";
+  const appId = "NZgHcC9ZnV8RB4meNsSkXQX85Zlg7la9vM4VxTYo";
+  Moralis.start({serverUrl, appId});
   return (
-    <div className="App">
-          <p>Learn React</p>
-    </div>
+    <Login />
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Moralis from 'moralis/dist/moralis.min.js';
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
+
+export default function Login() {
+    async function handleSubmit() {
+        let user = Moralis.User.current();
+        if (!user) {
+            user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+                .then(function (user) {
+                    // console.log("logged in user:", user);
+                    // console.log(user.get("ethAddress"));
+                })
+                .catch(function (error) {
+                    // console.log(error);
+                    // console.log(error);
+                });
+        } else {
+            // Handle logged in user
+        }
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: 'url(https://source.unsplash.com/random)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box component="form" sx={{ mt: 1 }}>
+                            <Button
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                onClick={handleSubmit}
+                            >
+                                Sign in with MetaMask
+                            </Button>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                href='https://metamask.io/download/'
+                                variant="outlined"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                 Download MetaMask
+                            </Button>
+                            <Copyright sx={{ mt: 5 }} />
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
+}
